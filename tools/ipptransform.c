a0d88df7e7bf22dae3bb67844106b039311374c0
Michael Sweet <mike@ma0103e-dhcp174.apple.com>
2016-02-18 08:27:02 -0800

    Save skeleton for ipptransform.

--- tools/ipptransform.c
+++ tools/ipptransform.c
@@ -23,15 +23,32 @@
  * Local types...
  */
 
-typedef struct xform_raster_s
+typedef ssize_t (*xform_write_cb_t)(void *, const void *, size_t);
+
+typedef struct xform_raster_s xform_raster_t;
+
+struct xform_raster_s
 {
-  cups_raster_t		*ras;		/* Raster stream */
+  int			num_options;	/* Number of job options */
+  cups_option_t		*options;	/* Job options */
   cups_page_header2_t	header;		/* Page header */
   unsigned char		*band_buffer;	/* Band buffer */
   unsigned		band_height;	/* Band height */
-} xform_raster_t;
 
-typedef ssize_t (*xform_write_cb_t)(void *, const void *, size_t);
+  /* Set by start_job callback */
+  cups_raster_t		*ras;		/* Raster stream */
+
+  /* Set by start_page callback */
+  unsigned		top, bottom;	/* Top and bottom lines */
+  unsigned		left, right;	/* Left and right pixels */
+
+  /* Callbacks */
+  void			(*end_job)(xform_raster_t *, xform_write_cb_t *, void *);
+  void			(*end_page)(xform_raster_t *, xform_write_cb_t *, void *);
+  void			(*start_job)(xform_raster_t *, xform_write_cb_t *, void *);
+  void			(*start_page)(xform_raster_t *, xform_write_cb_t *, void *);
+  void			(*write_line)(xform_raster_t *, unsigned, const unsigned char *, xform_write_cb_t *, void *);
+};
 
 
 /*
@@ -39,19 +56,23 @@ typedef ssize_t (*xform_write_cb_t)(void *, const void *, size_t);
  */
 
 static int	load_env_options(cups_option_t **options);
-static void	pcl_end_job(xform_write_cb_t cb, void *ctx);
+static void	pcl_end_job(xform_raster_t *ras, xform_write_cb_t cb, void *ctx);
 static void	pcl_end_page(xform_raster_t *ras, xform_write_cb_t cb, void *ctx);
-static void	pcl_start_job(xform_write_cb_t cb, void *ctx);
+static void	pcl_init(xform_raster_t *ras);
+static void	pcl_start_job(xform_raster_t *ras, xform_write_cb_t cb, void *ctx);
 static void	pcl_start_page(xform_raster_t *ras, xform_write_cb_t cb, void *ctx);
-static void	pcl_write_line(unsigned char *line, xform_raster_t *ras, xform_write_cb_t cb, void *ctx);
-static void	raster_end_job(xform_write_cb_t cb, void *ctx);
+static void	pcl_write_line(xform_raster_t *ras, unsigned y, unsigned char *line, xform_write_cb_t cb, void *ctx);
+static void	raster_end_job(xform_raster_t *ras, xform_write_cb_t cb, void *ctx);
 static void	raster_end_page(xform_raster_t *ras, xform_write_cb_t cb, void *ctx);
-static void	raster_start_job(xform_write_cb_t cb, void *ctx);
+static void	raster_init(xform_raster_t *ras);
+static void	raster_start_job(xform_raster_t *ras, xform_write_cb_t cb, void *ctx);
 static void	raster_start_page(xform_raster_t *ras, xform_write_cb_t cb, void *ctx);
-static void	raster_write_line(unsigned char *line, xform_raster_t *ras, xform_write_cb_t cb, void *ctx);
+static void	raster_write_line(xform_raster_t *ras, unsigned y, unsigned char *line, xform_write_cb_t cb, void *ctx);
 static void	usage(int status) __attribute__((noreturn));
+static ssize_t	write_fd(int *ctx, const void *buffer, size_t bytes);
 static int	xform_jpeg(const char *filename, int num_options, cups_option_t *options, xform_write_cb_t cb, void *ctx);
 static int	xform_pdf(const char *filename, int num_options, cups_option_t *options, xform_write_cb_t cb, void *ctx);
+static int	xform_setup(xform_raster_t *ras, const char *format, unsigned pages, int num_options, cups_option_t *options);
 
 
 /*
@@ -63,4 +84,238 @@ main(int  argc,
      char *argv[])
 {
   return (0);
+}
+
+
+/*
+ * 'load_env_options()' - Load options from the environment.
+ */
+
+static int				/* O - Number of options */
+load_env_options(
+    cups_option_t **options)		/* I - Options */
+{
+  int	num_options = 0;		/* Number of options */
+}
+
+
+/*
+ * 'pcl_end_job()' - End a PCL "job".
+ */
+
+static void
+pcl_end_job(xform_raster_t   *ras,	/* I - Raster information */
+            xform_write_cb_t cb,	/* I - Write callback */
+            void             *ctx)	/* I - Write context */
+{
+}
+
+
+/*
+ * 'pcl_end_page()' - End of PCL page.
+ */
+
+static void
+pcl_end_page(xform_raster_t   *ras,	/* I - Raster information */
+             xform_write_cb_t cb,	/* I - Write callback */
+             void             *ctx)	/* I - Write context */
+{
+}
+
+
+/*
+ * 'pcl_init()' - Initialize callbacks for PCL output.
+ */
+
+static void
+pcl_init(xform_raster_t *ras)		/* I - Raster information */
+{
+  ras->end_job    = pcl_end_job;
+  ras->end_page   = pcl_end_page;
+  ras->start_job  = pcl_start_job;
+  ras->start_page = pcl_start_page;
+  ras->write_line = pcl_write_line;
+}
+
+
+/*
+ * 'pcl_start_job()' - Start a PCL "job".
+ */
+
+static void
+pcl_start_job(xform_raster_t   *ras,	/* I - Raster information */
+              xform_write_cb_t cb,	/* I - Write callback */
+              void             *ctx)	/* I - Write context */
+{
+}
+
+
+/*
+ * 'pcl_start_page()' - Start a PCL page.
+ */
+
+static void
+pcl_start_page(xform_raster_t   *ras,	/* I - Raster information */
+               xform_write_cb_t cb,	/* I - Write callback */
+               void             *ctx)	/* I - Write context */
+{
+}
+
+
+/*
+ * 'pcl_write_line()' - Write a line of raster data.
+ */
+
+static void
+pcl_write_line(xform_raster_t   *ras,	/* I - Raster information */
+               unsigned         y,	/* I - Line number */
+               unsigned char    *line,	/* I - Pixels on line */
+               xform_write_cb_t cb,	/* I - Write callback */
+               void             *ctx)	/* I - Write context */
+{
+}
+
+
+/*
+ * 'raster_end_job()' - End a raster "job".
+ */
+
+static void
+raster_end_job(xform_raster_t   *ras,	/* I - Raster information */
+	       xform_write_cb_t cb,	/* I - Write callback */
+	       void             *ctx)	/* I - Write context */
+{
+}
+
+
+/*
+ * 'raster_end_page()' - End of raster page.
+ */
+
+static void
+raster_end_page(xform_raster_t   *ras,	/* I - Raster information */
+		xform_write_cb_t cb,	/* I - Write callback */
+		void             *ctx)	/* I - Write context */
+{
+}
+
+
+/*
+ * 'raster_init()' - Initialize callbacks for raster output.
+ */
+
+static void
+raster_init(xform_raster_t *ras)	/* I - Raster information */
+{
+  ras->end_job    = raster_end_job;
+  ras->end_page   = raster_end_page;
+  ras->start_job  = raster_start_job;
+  ras->start_page = raster_start_page;
+  ras->write_line = raster_write_line;
+}
+
+
+/*
+ * 'raster_start_job()' - Start a raster "job".
+ */
+
+static void
+raster_start_job(xform_raster_t   *ras,	/* I - Raster information */
+		 xform_write_cb_t cb,	/* I - Write callback */
+		 void             *ctx)	/* I - Write context */
+{
+}
+
+
+/*
+ * 'raster_start_page()' - Start a raster page.
+ */
+
+static void
+raster_start_page(xform_raster_t   *ras,/* I - Raster information */
+		  xform_write_cb_t cb,	/* I - Write callback */
+		  void             *ctx)/* I - Write context */
+{
+}
+
+
+/*
+ * 'raster_write_line()' - Write a line of raster data.
+ */
+
+static void
+raster_write_line(
+    xform_raster_t   *ras,		/* I - Raster information */
+    unsigned         y,			/* I - Line number */
+    unsigned char    *line,		/* I - Pixels on line */
+    xform_write_cb_t cb,		/* I - Write callback */
+    void             *ctx)		/* I - Write context */
+{
+}
+
+
+/*
+ * 'usage()' - Show program usage.
+ */
+
+static void
+usage(int status)			/* I - Exit status */
+{
+  puts("Usage: ipptransform filename [options]");
+
+  exit(status);
+}
+
+
+/*
+ * 'write_fd()' - Write to a file/socket.
+ */
+
+static ssize_t				/* O - Number of bytes written or -1 on error */
+write_fd(int        *ctx,		/* I - File descriptor */
+         const void *buffer,		/* I - Buffer */
+         size_t     bytes)		/* I - Number of bytes to write */
+{
+}
+
+
+/*
+ * 'xform_jpeg()' - Transform a JPEG image for printing.
+ */
+
+static int				/* O - 1 on success, 0 on error */
+xform_jpeg(const char       *filename,	/* I - File to transform */
+           int              num_options,/* I - Number of options */
+           cups_option_t    *options,	/* I - Options */
+           xform_write_cb_t cb,		/* I - Write callback */
+           void             *ctx)	/* I - Write context */
+{
+}
+
+
+/*
+ * 'xform_pdf()' - Transform a PDF file for printing.
+ */
+
+static int				/* O - 1 on success, 0 on error */
+xform_pdf(const char       *filename,	/* I - File to transform */
+          int              num_options,	/* I - Number of options */
+          cups_option_t    *options,	/* I - Options */
+          xform_write_cb_t cb,		/* I - Write callback */
+          void             *ctx)	/* I - Write context */
+{
+}
+
+
+/*
+ * 'xform_setup()' - Setup a raster context for printing.
+ */
+
+static int				/* O - 1 on success, 0 on failure */
+xform_setup(xform_raster_t *ras,	/* I - Raster information */
+            const char     *format,	/* I - Output format (MIME media type) */
+            unsigned       pages,	/* I - Number of pages */
+            int            num_options,	/* I - Number of options */
+            cups_option_t  *options)	/* I - Options */
+{
 }
