677e131a3c89ed6e1ec1b455a4ff80b1b057b8bc
Rithvik Patibandla <rithvikp98@gmail.com>
2018-03-15 17:59:01 +0530

    Start parsing PWG raster file (Issue #29)

--- tools/ippdoclint.c
+++ tools/ippdoclint.c
@@ -241,6 +241,73 @@ lint_pdf(const char    *filename,	/* I - File to check */
   return (1);
 }
 
+char when_enum[5][20] = {
+  "Never",
+  "AfterDocument",
+  "AfterJob",
+  "AfterSet",
+  "AfterPage"
+};
+
+char media_position_enum[50][20] = {
+  "Auto",
+  "Main",
+  "Alternate",
+  "LargeCapacity",
+  "Manual",
+  "Envelope",
+  "Disc",
+  "Photo",
+  "Hagaki",
+  "MainRoll",
+  "AlternateRoll",
+  "Top",
+  "Middle",
+  "Bottom",
+  "Side",
+  "Left",
+  "Right",
+  "Center",
+  "Rear",
+  "ByPassTray",
+  "Tray1",
+  "Tray2",
+  "Tray3",
+  "Tray4",
+  "Tray5",
+  "Tray6",
+  "Tray7",
+  "Tray8",
+  "Tray9",
+  "Tray10",
+  "Tray11",
+  "Tray12",
+  "Tray13",
+  "Tray14",
+  "Tray15",
+  "Tray16",
+  "Tray17",
+  "Tray18",
+  "Tray19",
+  "Tray20",
+  "Roll1",
+  "Roll2",
+  "Roll3",
+  "Roll4",
+  "Roll5",
+  "Roll6",
+  "Roll7",
+  "Roll8",
+  "Roll9",
+  "Roll10",
+};
+
+char orientation_enum[4][20] = {
+  "Portrait",
+  "Landscape",
+  "ReversePortrait",
+  "ReverseLandscape"
+};
 
 /*
  * 'lint_raster()' - Check an Apple/CUPS/PWG Raster file.
@@ -270,10 +337,145 @@ lint_raster(const char    *filename,	/* I - File to check */
   * - job-pages-completed-col
   */
 
-  (void)filename;
+  FILE *file = fopen(filename, "rb");
+  rewind(file);
+  
+  char sync_word[4];
+  fscanf(file, "%4s", sync_word);
+  
+  if(strcmp(sync_word, "RaS2")){
+    fprintf(stderr, "ERROR: Synchronization word mismatch\n");
+    return (1);
+  }
+  fprintf(stderr, "DEBUG: Synchronization word is correct\n");
+  
+  cups_page_header2_t header;
+  fread(&header, 1, 1796, file);
+  
+  if(strncmp(header.MediaClass, "PwgRaster", 64)){
+    fprintf(stderr, "ERROR: PwgRaster value in header is incorrect\n");
+    return (1);
+  }
+  fprintf(stderr, "DEBUG: Header value PwgRaster is correct\n");
+  
+  if (header.MediaColor[0]=='\0')
+    fprintf(stderr, "DEBUG: Using default value for MediaColor\n");
+  else 
+    fprintf(stderr, "DEBUG: Using value %s for MediaColor", header.MediaColor);
+  
+  if (header.MediaType[0]=='\0')
+    fprintf(stderr, "DEBUG: Using default value for MediaType\n");
+  else 
+    fprintf(stderr, "DEBUG: Using value %s for MediaType", header.MediaType);
+
+  if (header.OutputType[0]=='\0')
+    fprintf(stderr, "DEBUG: Using default value for PrintContentOptimize\n");
+  else 
+    fprintf(stderr, "DEBUG: Using value %s for PrintContentOptimize", header.OutputType);
+
+  if (header.AdvanceMedia != 0 || header.Collate != 0){
+    fprintf(stderr, "ERROR: Non-zero values present in Reserved[256-267] area\n");
+    return(1);
+  }
+  else
+    fprintf(stderr, "DEBUG: Reserved[256-267] field is zero as expected\n");
+
+  if (header.CutMedia < 0 || header.CutMedia > 4) {
+    fprintf(stderr, "ERROR: Incorrect value present for CutMedia\n");
+    return(1);
+  }
+  fprintf(stderr, "DEBUG: Value of CutMedia is %d(%s)\n", header.CutMedia, when_enum[header.CutMedia]);
+
+  if(header.Duplex == 0)
+    fprintf(stderr, "DEBUG: Duplex mode off\n");
+  else if(header.Duplex == 1)
+    fprintf(stderr, "DEBUG: Duplex mode on\n");
+  else
+    fprintf(stderr, "DEBUG: Incorrect Duplex value\n");
+
+  /* [TODO]: Not working properly */
+  fprintf(stderr, "DEBUG: Using cross-feed resolution of %u and feed resolution of %u\n", header.HWResolution[0], header.HWResolution[1]);
+
+  /* [TODO]: Not working properly */
+  for(int i=0; i<4; i++){
+    if(header.ImagingBoundingBox[i]!=0){
+      fprintf(stderr, "ERROR: Non-zero values present in Reserved[284-299] area\n");
+      //return(1);
+    }
+  }
+  fprintf(stderr, "DEBUG: Reserved[284-299] field is zero as expected\n");
+  
+  if(header.InsertSheet == 0)
+    fprintf(stderr, "DEBUG: InsertSheet set to false\n");
+  else if(header.InsertSheet == 1)
+    fprintf(stderr, "DEBUG: InsertSheet set to true\n");
+  else
+    fprintf(stderr, "DEBUG: Incorrect InsertSheet value\n");
+
+  if (header.Jog < 0 || header.Jog > 4) {
+    fprintf(stderr, "ERROR: Incorrect value present for Jog %d\n", header.Jog);
+    return(1);
+  }
+  fprintf(stderr, "DEBUG: Value of Jog is %d(%s)\n", header.Jog, when_enum[header.Jog]);
+  
+  if(header.LeadingEdge == 0)
+    fprintf(stderr, "DEBUG: LeadingEdge set to ShortEdgeFirst\n");
+  else if(header.LeadingEdge == 1)
+    fprintf(stderr, "DEBUG: LeadingEdge set to LongEdgeFirst\n");
+  else
+    fprintf(stderr, "DEBUG: Incorrect LeadingEdge value\n");
+
+  if (header.Margins[0] != 0 || header.Margins[1] != 0 || header.ManualFeed != 0){
+    fprintf(stderr, "ERROR: Non-zero values present in Reserved[312-323] area\n");
+    return(1);
+  }
+  else
+    fprintf(stderr, "DEBUG: Reserved[312-323] field is zero as expected\n");
+
+  if (header.MediaPosition < 0 || header.MediaPosition > 49) {
+    fprintf(stderr, "ERROR: Incorrect value present for MediaPosition\n");
+    return(1);
+  }
+  fprintf(stderr, "DEBUG: Value of MediaPosition is %d(%s)\n", header.MediaPosition, media_position_enum[header.MediaPosition]);
+
+  if (header.MediaWeight==0)
+    fprintf(stderr, "DEBUG: Using default value for MediaWeight\n");
+  else 
+    fprintf(stderr, "DEBUG: Using value %d for MediaWeight", header.MediaWeight);
+
+  if (header.MirrorPrint != 0 || header.NegativePrint != 0){
+    fprintf(stderr, "ERROR: Non-zero values present in Reserved[332-339] area\n");
+    return(1);
+  }
+  else
+    fprintf(stderr, "DEBUG: Reserved[332-339] field is zero as expected\n");
+
+  /* [TODO]: Not working properly */
+  if (header.NumCopies==0)
+    fprintf(stderr, "DEBUG: Using default value for NumCopies\n");
+  else 
+    fprintf(stderr, "DEBUG: Using value %d for NumCopies\n", header.NumCopies);
+
+  if (header.Orientation < 0 || header.Orientation > 4) {
+    fprintf(stderr, "ERROR: Incorrect value present for Orientation %d\n", header.Orientation);
+    return(1);
+  }
+  fprintf(stderr, "DEBUG: Value of Orientation is %d(%s)\n", header.Orientation, orientation_enum[header.Orientation]);
+  
+  if (header.OutputFaceUp != 0){
+    fprintf(stderr, "ERROR: Non-zero values present in Reserved[348-351] area\n");
+    return(1);
+  }
+  else
+    fprintf(stderr, "DEBUG: Reserved[348-351] field is zero as expected\n");
+
+  /* [TODO]: Not working properly */ 
+  fprintf(stderr, "DEBUG: Page size is %d x %d\n", header.PageSize[0], header.PageSize[1]);
+
   (void)num_options;
   (void)options;
 
+  fclose(file);
   return (1);
 }
 
