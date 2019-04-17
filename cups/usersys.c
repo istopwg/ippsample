810762817d1e1105608435f5cfea6f72a18852f0
Michael R Sweet <michael.r.sweet@gmail.com>
2019-04-17 07:50:59 -0400

    Sync up libcups changes from CUPS master@4d2df926779f3d4aba62f05c57ab703c1e0470e3

--- cups/usersys.c
+++ cups/usersys.c
@@ -1,10 +1,11 @@
 /*
  * User, system, and password routines for CUPS.
  *
- * Copyright 2007-2017 by Apple Inc.
+ * Copyright 2007-2019 by Apple Inc.
  * Copyright 1997-2006 by Easy Software Products.
  *
- * Licensed under Apache License v2.0.  See the file "LICENSE" for more information.
+ * Licensed under Apache License v2.0.  See the file "LICENSE" for more
+ * information.
  */
 
 /*
@@ -32,13 +33,14 @@
  */
 
 #ifdef __APPLE__
-#  if TARGET_OS_IOS
-#    define kCUPSPrintingPrefs	CFSTR(".GlobalPreferences")
-#    define kPREFIX		"AirPrint"
-#  else
+#  if TARGET_OS_OSX
 #    define kCUPSPrintingPrefs	CFSTR("org.cups.PrintingPrefs")
 #    define kPREFIX		""
-#  endif /* TARGET_OS_IOS */
+#  else
+#    define kCUPSPrintingPrefs	CFSTR(".GlobalPreferences")
+#    define kPREFIX		"AirPrint"
+#  endif /* TARGET_OS_OSX */
+#  define kUserAgentTokensKey	CFSTR(kPREFIX "UserAgentTokens")
 #  define kAllowAnyRootKey	CFSTR(kPREFIX "AllowAnyRoot")
 #  define kAllowExpiredCertsKey	CFSTR(kPREFIX "AllowExpiredCerts")
 #  define kEncryptionKey	CFSTR(kPREFIX "Encryption")
@@ -61,6 +63,7 @@
 
 typedef struct _cups_client_conf_s	/**** client.conf config data ****/
 {
+  _cups_uatokens_t	uatokens;	/* UserAgentTokens values */
 #ifdef HAVE_SSL
   int			ssl_options,	/* SSLOptions values */
 			ssl_min_version,/* Minimum SSL/TLS version */
@@ -102,6 +105,7 @@ static void	cups_set_server_name(_cups_client_conf_t *cc, const char *value);
 #ifdef HAVE_SSL
 static void	cups_set_ssl_options(_cups_client_conf_t *cc, const char *value);
 #endif /* HAVE_SSL */
+static void	cups_set_uatokens(_cups_client_conf_t *cc, const char *value);
 static void	cups_set_user(_cups_client_conf_t *cc, const char *value);
 
 
@@ -517,6 +521,29 @@ cupsSetUserAgent(const char *user_agent)/* I - User-Agent string or @code NULL@ 
     return;
   }
 
+  if (cg->uatokens < _CUPS_UATOKENS_OS)
+  {
+    switch (cg->uatokens)
+    {
+      default :
+      case _CUPS_UATOKENS_NONE :
+	  cg->user_agent[0] = '\0';
+	  break;
+      case _CUPS_UATOKENS_PRODUCT_ONLY :
+	  strlcpy(cg->user_agent, "CUPS IPP", sizeof(cg->user_agent));
+	  break;
+      case _CUPS_UATOKENS_MAJOR :
+	  snprintf(cg->user_agent, sizeof(cg->user_agent), "CUPS/%d IPP/2", CUPS_VERSION_MAJOR);
+	  break;
+      case _CUPS_UATOKENS_MINOR :
+	  snprintf(cg->user_agent, sizeof(cg->user_agent), "CUPS/%d.%d IPP/2.1", CUPS_VERSION_MAJOR, CUPS_VERSION_MINOR);
+	  break;
+      case _CUPS_UATOKENS_MINIMAL :
+	  strlcpy(cg->user_agent, CUPS_MINIMAL " IPP/2.1", sizeof(cg->user_agent));
+	  break;
+    }
+  }
+
 #ifdef _WIN32
  /*
   * Gather Windows version information for the User-Agent string...
@@ -549,7 +576,10 @@ cupsSetUserAgent(const char *user_agent)/* I - User-Agent string or @code NULL@ 
         break;
   }
 
-  snprintf(cg->user_agent, sizeof(cg->user_agent), CUPS_MINIMAL " (Windows %d.%d; %s) IPP/2.0", version.dwMajorVersion, version.dwMinorVersion, machine);
+  if (cg->uatokens == _CUPS_UATOKENS_OS)
+    snprintf(cg->user_agent, sizeof(cg->user_agent), CUPS_MINIMAL " (Windows %d.%d) IPP/2.0", version.dwMajorVersion, version.dwMinorVersion);
+  else
+    snprintf(cg->user_agent, sizeof(cg->user_agent), CUPS_MINIMAL " (Windows %d.%d; %s) IPP/2.0", version.dwMajorVersion, version.dwMinorVersion, machine);
 
 #elif defined(__APPLE__)
  /*
@@ -564,11 +594,18 @@ cupsSetUserAgent(const char *user_agent)/* I - User-Agent string or @code NULL@ 
   else
     strlcpy(version, "unknown", sizeof(version));
 
-#  if TARGET_OS_IOS
-  snprintf(cg->user_agent, sizeof(cg->user_agent), CUPS_MINIMAL " (iOS %s; %s) IPP/2.0", version, name.machine);
+#  if TARGET_OS_OSX
+  if (cg->uatokens == _CUPS_UATOKENS_OS)
+    snprintf(cg->user_agent, sizeof(cg->user_agent), CUPS_MINIMAL " (macOS %s) IPP/2.0", version);
+  else
+    snprintf(cg->user_agent, sizeof(cg->user_agent), CUPS_MINIMAL " (macOS %s; %s) IPP/2.0", version, name.machine);
+
 #  else
-  snprintf(cg->user_agent, sizeof(cg->user_agent), CUPS_MINIMAL " (macOS %s; %s) IPP/2.0", version, name.machine);
-#  endif /* TARGET_OS_IOS */
+  if (cg->uatokens == _CUPS_UATOKENS_OS)
+    snprintf(cg->user_agent, sizeof(cg->user_agent), CUPS_MINIMAL " (iOS %s) IPP/2.0", version);
+  else
+    snprintf(cg->user_agent, sizeof(cg->user_agent), CUPS_MINIMAL " (iOS %s; %s) IPP/2.0", version, name.machine);
+#  endif /* TARGET_OS_OSX */
 
 #else
  /*
@@ -577,7 +614,10 @@ cupsSetUserAgent(const char *user_agent)/* I - User-Agent string or @code NULL@ 
 
   uname(&name);
 
-  snprintf(cg->user_agent, sizeof(cg->user_agent), CUPS_MINIMAL " (%s %s; %s) IPP/2.0", name.sysname, name.release, name.machine);
+  if (cg->uatokens == _CUPS_UATOKENS_OS)
+    snprintf(cg->user_agent, sizeof(cg->user_agent), CUPS_MINIMAL " (%s %s) IPP/2.0", name.sysname, name.release);
+  else
+    snprintf(cg->user_agent, sizeof(cg->user_agent), CUPS_MINIMAL " (%s %s; %s) IPP/2.0", name.sysname, name.release, name.machine);
 #endif /* _WIN32 */
 }
 
@@ -979,6 +1019,8 @@ _cupsSetDefaults(void)
 
   cups_finalize_client_conf(&cc);
 
+  cg->uatokens = cc.uatokens;
+
   if (cg->encryption == (http_encryption_t)-1)
     cg->encryption = cc.encryption;
 
@@ -1136,17 +1178,22 @@ cups_finalize_client_conf(
 
   if (!cc->server_name[0])
   {
-#ifdef CUPS_DEFAULT_DOMAINSOCKET
    /*
     * If we are compiled with domain socket support, only use the
     * domain socket if it exists and has the right permissions...
     */
 
+#if defined(__APPLE__) && !TARGET_OS_OSX
+    cups_set_server_name(cc, "/private/var/run/printd");
+
+#else
+#  ifdef CUPS_DEFAULT_DOMAINSOCKET
     if (!access(CUPS_DEFAULT_DOMAINSOCKET, R_OK))
       cups_set_server_name(cc, CUPS_DEFAULT_DOMAINSOCKET);
     else
-#endif /* CUPS_DEFAULT_DOMAINSOCKET */
-      cups_set_server_name(cc, "localhost");
+#  endif /* CUPS_DEFAULT_DOMAINSOCKET */
+    cups_set_server_name(cc, "localhost");
+#endif /* __APPLE__ && !TARGET_OS_OSX */
   }
 
   if (!cc->user[0])
@@ -1216,9 +1263,11 @@ cups_init_client_conf(
 
   memset(cc, 0, sizeof(_cups_client_conf_t));
 
-#if TARGET_OS_IOS
+  cc->uatokens = _CUPS_UATOKENS_MINIMAL;
+
+#if defined(__APPLE__) && !TARGET_OS_OSX
   cups_set_user(cc, "mobile");
-#endif /* TARGET_OS_IOS */
+#endif /* __APPLE__ && !TARGET_OS_OSX */
 
 #ifdef HAVE_SSL
   cc->ssl_min_version = _HTTP_TLS_1_0;
@@ -1235,8 +1284,9 @@ cups_init_client_conf(
   * everything...)
   */
 
-#if defined(__APPLE__) && defined(HAVE_SSL)
+#if defined(__APPLE__)
   char	sval[1024];			/* String value */
+#  ifdef HAVE_SSL
   int	bval;				/* Boolean value */
 
   if (cups_apple_get_boolean(kAllowAnyRootKey, &bval))
@@ -1272,7 +1322,13 @@ cups_init_client_conf(
 
   if (cups_apple_get_boolean(kValidateCertsKey, &bval))
     cc->validate_certs = bval;
-#endif /* __APPLE__ && HAVE_SSL */
+#  endif /* HAVE_SSL */
+
+  if (cups_apple_get_string(kUserAgentTokensKey, sval, sizeof(sval)))
+  {
+    cups_set_uatokens(cc, sval);
+  }
+#endif /* __APPLE__ */
 }
 
 
@@ -1309,6 +1365,8 @@ cups_read_client_conf(
 #endif /* !__APPLE__ */
     else if (!_cups_strcasecmp(line, "User") && value)
       cups_set_user(cc, value);
+    else if (!_cups_strcasecmp(line, "UserAgentTokens") && value)
+      cups_set_uatokens(cc, value);
     else if (!_cups_strcasecmp(line, "TrustOnFirstUse") && value)
       cc->trust_first = cups_boolean_value(value);
     else if (!_cups_strcasecmp(line, "AllowAnyRoot") && value)
@@ -1476,6 +1534,38 @@ cups_set_ssl_options(
   DEBUG_printf(("4cups_set_ssl_options(cc=%p, value=\"%s\") options=%x, min_version=%d, max_version=%d", (void *)cc, value, options, min_version, max_version));
 }
 #endif /* HAVE_SSL */
+
+
+/*
+ * 'cups_set_uatokens()' - Set the UserAgentTokens value.
+ */
+
+static void
+cups_set_uatokens(
+    _cups_client_conf_t *cc,		/* I - client.conf values */
+    const char          *value)		/* I - Value */
+{
+  int	i;				/* Looping var */
+  static const char * const uatokens[] =/* UserAgentTokens values */
+  {
+    "NONE",
+    "PRODUCTONLY",
+    "MAJOR",
+    "MINOR",
+    "MINIMAL",
+    "OS",
+    "FULL"
+  };
+
+  for (i = 0; i < (int)(sizeof(uatokens) / sizeof(uatokens[0])); i ++)
+  {
+    if (!_cups_strcasecmp(value, uatokens[i]))
+    {
+      cc->uatokens = (_cups_uatokens_t)i;
+      return;
+    }
+  }
+}
 
 
 /*
