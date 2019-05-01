5d07dbaedda7c79806d1c9c2a124589836af2ab7
Michael R Sweet <michael.r.sweet@gmail.com>
2019-05-01 15:48:10 -0400

    Update script that updates the CUPS library and tools.

--- scripts/update-cups.sh
+++ scripts/update-cups.sh
@@ -16,5 +16,5 @@ fi
 oldrev=`cat .cups-upstream`
 newrev=`cd ../cups; git show | head -1 | awk '{print $2}'`
 
-(cd ../cups; git diff $oldrev cups ':!cups/Dependencies' ':!cups/Makefile' ':!cups/libcups2.def' ':!cups/adminutil*' ':!cups/backchannel*' ':!cups/getifaddrs*' ':!cups/interpret*' ':!cups/ppd*' ':!cups/raster-inter*' ':!cups/sidechannel*' ':!cups/test*' ':!cups/snmp*' ':!cups/tlscheck.c' ':!cups/cupspm*' ':!cups/*.shtml' ':!cups/*.header') >$newrev.patch
+(cd ../cups; git diff $oldrev cups man/ipp*.[15] tools ':!*/Dependencies' ':!*/Makefile' ':!cups/libcups2.def' ':!cups/adminutil*' ':!cups/backchannel*' ':!cups/getifaddrs*' ':!cups/interpret*' ':!cups/ppd*' ':!cups/raster-inter*' ':!cups/sidechannel*' ':!cups/test*' ':!cups/snmp*' ':!cups/tlscheck.c' ':!cups/cupspm*' ':!cups/*.shtml' ':!cups/*.header') >$newrev.patch
 git apply $newrev.patch && (echo $newrev >.cups-upstream; git commit -a -m "Sync up libcups changes from CUPS master@$newrev"; rm -f $newrev.patch) || echo "$newrev.patch did not apply."
