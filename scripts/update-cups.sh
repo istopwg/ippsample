71580355ba616f153d8cbe5baff5086d08b15a77
Michael R Sweet <michael.r.sweet@gmail.com>
2019-05-01 15:52:47 -0400

    Clean up commit message.

--- scripts/update-cups.sh
+++ scripts/update-cups.sh
@@ -17,4 +17,4 @@ oldrev=`cat .cups-upstream`
 newrev=`cd ../cups; git show | head -1 | awk '{print $2}'`
 
 (cd ../cups; git diff $oldrev cups man/ipp*.[15] tools ':!*/Dependencies' ':!*/Makefile' ':!cups/libcups2.def' ':!cups/adminutil*' ':!cups/backchannel*' ':!cups/getifaddrs*' ':!cups/interpret*' ':!cups/ppd*' ':!cups/raster-inter*' ':!cups/sidechannel*' ':!cups/test*' ':!cups/snmp*' ':!cups/tlscheck.c' ':!cups/cupspm*' ':!cups/*.shtml' ':!cups/*.header') >$newrev.patch
-git apply $newrev.patch && (echo $newrev >.cups-upstream; git commit -a -m "Sync up libcups changes from CUPS master@$newrev"; rm -f $newrev.patch) || echo "$newrev.patch did not apply."
+git apply $newrev.patch && (echo $newrev >.cups-upstream; git commit -a -m "Merge changes from CUPS master@$newrev"; rm -f $newrev.patch) || echo "$newrev.patch did not apply."
